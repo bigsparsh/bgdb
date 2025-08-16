c325b3e745416acaeb4594f1424b663162aca53d
Sparsh Singh <sparshsingh7586@gmail.com>
2025-08-16 20:28:17 +0530

    Added cursor

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <sys/types.h>
 
 #define COLUMN_USERNAME_SIZE 40
@@ -30,28 +33,45 @@ const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 
+// Used for taking input and maintaing the input size and length.
 typedef struct {
 	char* buffer;
 	size_t buffer_length;
 	ssize_t buffer_size;
 } input_buffer;
 
+// Used as a pager instance for the DB file, maintains the file length and all the pages within that are to be loaded in memory or freed as required.
+typedef struct {
+	int file_descriptor;
+	uint32_t file_length;
+	void *pages[TABLE_MAX_PAGES];
+} pager;
 
+// A table instance, conatining number of rows and a pager instance.
 typedef struct {
 	uint32_t num_rows;
-	void *pages[TABLE_MAX_PAGES];
+	pager* pgr;
 } table;
 
+typedef struct {
+	table *tbl;
+	uint32_t row_num;
+	bool end_of_tbl;
+} cursor;
+
+// Determining the normal command success or unrecognized.
 typedef enum {
 	COMMAND_SUCCESS,
 	COMMAND_UNRECOGNIZED
 } command_result;
 
+// Determinig the execution status of SQL Commands.
 typedef enum {
 	EXECUTE_SUCCESS,
 	EXECUTE_TABLE_FULL
 } execute_result;
 
+// Determining the preparation status for the commands.
 typedef enum {
 	PREPARE_SUCCESS,
 	PREPARE_UNRECOGNIZED_STATEMENT,
@@ -60,24 +80,107 @@ typedef enum {
 	PREPARE_SYNTAX_ERROR
 } prepare_result;
 
+// Defining the type of SQL Statement.
 typedef enum {
 	STATEMENT_INSERT,
 	STATEMENT_SELECT
 } statement_type;
 
+// A statement instance, containing the row instance which is to be inserted as well as the type of the statement.
 typedef struct {
 	statement_type type;
 	row row_to_insert;
 } statement;
 
 
+// Kills the input buffer, i.e, frees the Input Buffer's buffer string and also the Input Buffer instance itself.
 void kill_input_buffer (input_buffer* input_bfr) {
 	free(input_bfr->buffer);
 	free(input_bfr);
 }
 
-command_result do_command (input_buffer *input_bfr) {
+void pager_flush (pager* pgr, uint32_t page_num, uint32_t size) {
+	if (pgr->pages[page_num] == NULL) {
+		printf("Tried to flush null page.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	off_t offset = lseek(pgr->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+
+	if (offset == -1) {
+		printf("Error seeking: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+
+	ssize_t bytes_written = write(pgr->file_descriptor, pgr->pages[page_num], size);
+
+	if (bytes_written == -1) {
+		printf("Error writing: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+}
+
+// Closes the Database by freeing the pages from the memory.
+void db_close (table* tbl) {
+	pager* pgr = tbl->pgr;
+	uint32_t num_full_pages = tbl->num_rows / ROWS_PER_PAGE;
+
+	for (uint32_t i = 0; i < num_full_pages; i++) {
+		if (pgr->pages[i] == NULL) {
+			continue;
+		}
+		pager_flush(pgr, i, PAGE_SIZE);
+		free(pgr->pages[i]);
+		pgr->pages[i] = NULL;
+	}
+
+	uint32_t num_additional_rows = tbl->num_rows % ROWS_PER_PAGE;
+	if (num_additional_rows > 0) {
+		uint32_t page_num = num_full_pages;
+		if (pgr->pages[page_num] != NULL) {
+			pager_flush(pgr, page_num, num_additional_rows * ROW_SIZE);
+			free(pgr->pages[page_num]);
+			pgr->pages[page_num] = NULL;
+		}
+	}
+
+	int result = close(pgr->file_descriptor);
+	if (result == -1) {
+		printf("Error closing db file.\n");
+		exit(EXIT_FAILURE);
+	}
+	for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+		void *page = pgr->pages[i];
+		if (page) {
+			free(page);
+			pgr->pages[i] = NULL;
+		}
+	}
+	free(pgr);
+	free(tbl);
+}
+
+cursor* table_start(table* tbl) {
+	cursor* crsr = malloc(sizeof(cursor));
+	crsr->tbl = tbl;
+	crsr->row_num = 0;
+	crsr->end_of_tbl = (tbl->num_rows == 0);
+
+	return crsr;
+}
+
+cursor* table_end(table* tbl) {
+	cursor* crsr = malloc(sizeof(cursor));
+	crsr->tbl = tbl;
+	crsr->row_num = tbl->num_rows;
+	crsr->end_of_tbl = true;
+
+	return crsr;
+}
+
+command_result do_command (input_buffer *input_bfr, table* tbl) {
 	if (strcmp(input_bfr->buffer, ".exit") == 0) {
+		db_close(tbl);
 		exit(EXIT_SUCCESS);
 	} else {
 		return (COMMAND_UNRECOGNIZED);
@@ -118,16 +221,49 @@ void deserialize_row (void* src, row* dest) {
 	memcpy(&(dest->email), src + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
-void *row_slot (table *tbl, uint32_t row_num) {
-	uint32_t page_num = row_num / ROWS_PER_PAGE;
-	void* page = tbl->pages[page_num];
+void* get_page (pager *pgr, uint32_t page_num) {
+	if (page_num > TABLE_MAX_PAGES) {
+		printf("Tried to fetch page number out of bounds. %d > %d\n", page_num, TABLE_MAX_PAGES);
+		exit(EXIT_FAILURE);
+	}
+	if (pgr->pages[page_num] == NULL) {
+		void *page = malloc(PAGE_SIZE);
+		uint32_t num_pages = pgr->file_length / PAGE_SIZE;
 
-	if (page == NULL) page = tbl->pages[page_num] = malloc(PAGE_SIZE);
+		if (pgr->file_length % PAGE_SIZE) num_pages += 1;
+
+		if (page_num <= num_pages) {
+			lseek(pgr->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+			ssize_t bytes_read = read(pgr->file_descriptor, page, PAGE_SIZE);
+			if (bytes_read == -1) {
+				printf("Error reading file: %d\n", errno);
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		pgr->pages[page_num] = page;
+	}
+
+	return pgr->pages[page_num];
+}
+
+void *cursor_value (cursor *crsr) {
+	uint32_t row_num = crsr->row_num;
+	uint32_t page_num = row_num / ROWS_PER_PAGE;
+
+	void *page = get_page(crsr->tbl->pgr, page_num);
+
 	uint32_t row_offset = row_num % ROWS_PER_PAGE;
 	uint32_t byte_offset = row_offset * ROW_SIZE;
 	return page + byte_offset;
 }
 
+void cursor_advance (cursor* crsr) {
+	crsr-> row_num += 1;
+	if (crsr->row_num >= crsr->tbl->num_rows) {
+		crsr->end_of_tbl  = true;
+	}
+}
 
 input_buffer* create_input_buffer () {
 	input_buffer* input_bfr = malloc(sizeof(input_buffer));
@@ -214,27 +350,49 @@ execute_result execute_statement (statement *stmt, table *tbl) {
 	return EXECUTE_SUCCESS;
 }
 
-table* new_table () {
-	table* tbl = ( table* ) malloc(sizeof(table));
-	tbl->num_rows = 0;
+pager* pager_open (const char* filename) {
+	int fd = open(filename, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR);
+
+	if (fd == -1) {
+		printf("Unable to open file.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	off_t file_length = lseek(fd, 0, SEEK_END);
+
+	pager* pgr = malloc(sizeof(pager));
+	pgr->file_descriptor = fd;
+	pgr->file_length = file_length;
 
 	for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
-		tbl->pages[i] = NULL;
+		pgr->pages[i] = NULL;
 	}
+
+	return pgr;
+}
+
+table* db_open (const char* filename) {
+	pager* pgr = pager_open(filename);
+	uint32_t num_rows = pgr->file_length / ROW_SIZE;
+	table* tbl = ( table* ) malloc(sizeof(table));
+
+	tbl->pgr = pgr;
+	tbl->num_rows = num_rows;
 
 	return tbl;
 }
 
-void free_table (table *tbl) {
-	for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
-		free( tbl->pages[i] );
+
+
+int main (int argc, char* argv[]) {
+
+	if (argc < 2) {
+		printf("Must supply a database filename.\n");
+		exit(EXIT_FAILURE);
 	}
-	free(tbl);
-}
 
-int main () {
-
-	table *tbl = new_table();
+	char *filename = argv[1];
+	table *tbl = db_open(filename);
 	input_buffer* input_bfr = create_input_buffer();
 
 	while (true) {
@@ -243,7 +401,7 @@ int main () {
 
 		if (strcmp(input_bfr->buffer, ".exit") == 0) {
 			if (input_bfr->buffer[0] == '.') {
-				switch (do_command(input_bfr)) {
+				switch (do_command(input_bfr, tbl)) {
 					case COMMAND_SUCCESS:
 						continue;
 					case COMMAND_UNRECOGNIZED:
